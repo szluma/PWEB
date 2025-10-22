a43b7d8022e34c759bd8c5a55a76fce714ecb473
luma <lumadesouzaferreira1@gmail.com>
2025-10-22 11:34:15 -0300

    Add files via upload

--- Projeto01 - PWEB/script.js
+++ Projeto01 - PWEB/script.js
@@ -15,7 +15,49 @@ else {
 
 cumprimento.innerHTML = `${hora}, <b>Clara e Luma👋</b>`;
 
-function adicionar_tarefa (){
-    const tarefas = document.getElementById("add_tarefa");
+// function adicionar_tarefa (){
+//     const tarefas = document.getElementById("add_tarefa");
+//     const meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+// }
+
+const form = document.getElementById("container_1");
+const verificado = document.getElementsByClassName("verificado");
+const tarefas = [];
+
+let ult_div = document.getElementById("ult_div");
+let copia_ult_div = ult_div;
+
+form.addEventListener("submit", function(CL){
+    CL.preventDefault();
+
+    let nome = document.getElementById("add_tarefa").value;
     const meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
-}
+    const data_tarefa = `${horas.getDate()} de ${meses[horas.getMonth() - 1]} ${horas.getFullYear()}`;
+    const situacao = false;
+
+    const tarefa = {
+        "nome": nome,
+        "data": data_tarefa,
+        "done": situacao 
+    };
+
+    tarefas.push(tarefa);
+    if(ult_div){
+        copia_ult_div.remove();
+    }
+
+    const tarefa_concluida = tarefas.filter(tarefa => tarefa.done === true). length;
+    let concluidas = document.getElementById("concluidas");
+
+    if(concluidas){
+        concluidas.innerHTML = `<h4>${tarefa_concluida} de ${tarefas.length} <b>concluídas</b>`;
+    }
+    else {
+        let concluidasEl = document.createElement("div");
+        concluidasEl.setAttribute("id", "concluidas");
+        document.getElementById("tarefasConcluidas").append(concluidasEl);
+        concluidasEl.innerHTML = `<h4>${tarefa_concluida} de ${tarefas.length} <b>concluídas</b>`;
+    }
+
+
+});
