cb83f25065c7db3cdf096ee9f9a5bff5c0d418f1
luma <lumadesouzaferreira1@gmail.com>
2025-11-29 00:22:16 -0300

    Add files via upload

--- pweb blog(teste)/scripts/posts.js
+++ pweb blog(teste)/scripts/posts.js
@@ -1,5 +1,7 @@
 const API_URL = "http://localhost:3000/usuarios";
 
+// botão de criar postagem
+
 const bt = document.getElementById("bt");
 const modal = document.getElementById("modal-overlay");
 
@@ -12,3 +14,21 @@ modal.addEventListener("click", (e)=> {
         modal.classList.remove("active");
     }
 })
+
+// botão de editar postagem
+
+const nomedt = document.getElementById("nomedt");
+const title = document.getElementById("title");
+const categoriaedt = document.getElementById("categoriaedt");
+const urledt = document.getElementById("urledt");
+const edtassunto = document.getElementById("edtassunto");
+
+let currentPost = null;
+
+edtmodaloverlay.addEventListener("click", (e)=> {
+    if(e.target === edtmodaloverlay){
+        edtmodaloverlay.classList.remove("active");
+        currentPost = null;
+    }
+})
+
